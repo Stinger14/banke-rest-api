822eff9d5fa2c99f9c73a7c62e2fa3203eb08565
Maxly Garcia <maxgarcia6@gmail.com>
2020-11-06 14:17:49 -0400

    Agregar autenticación de usuario para proteger las rutas de acceso a la API.

--- src/app.js
+++ src/app.js
@@ -1,22 +1,80 @@
-import express, { json } from 'express';
-import morgan from 'morgan';
+import express, { json, Router } from "express";
+import morgan from "morgan";
 
 // Importing routes
-import sucursalRoutes from './routes/adm/xadmsucm';
-import relacionadoRoutes from './routes/adm/xadmrelm';
-import solicitudRoutes from './routes/pre/epresolm';
+import sucursalRoutes from "./routes/adm/xadmsucm";
+import relacionadoRoutes from "./routes/adm/xadmrelm";
+import solicitudRoutes from "./routes/pre/epresolm";
+
+const bcrypt = require("bcrypt");
 
 // App init
 const app = express();
 
 // Middlewares
-app.use(morgan('dev')); // Show requests on console (debug)
-app.use(json());    // Understand json requests
+app.use(morgan("dev")); // Show requests on console (debug)
+app.use(json()); // Understand json requests
+
+app.set("view engine", "ejs"); // Set up ejs for rendering
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use('/api/sucursales', sucursalRoutes);
-app.use('/api/relacionados', relacionadoRoutes);
-app.use('/api/solicitudes', solicitudRoutes);
+app.get("/", (req, res) => {
+  res.render("index"); // main view -> /view/index.ejs
+});
 
+app.get("/users/register", (req, res) => {
+  res.render("register"); // main view -> /view/index.ejs
+});
+
+app.get("/users/login", (req, res) => {
+  res.render("login"); // main view -> /view/index.ejs
+});
+
+app.get("/users/dashboard", (req, res) => {
+  res.render("dashboard", { user: "Maxly" }); // main view -> /view/index.ejs
+});
+
+app.post("/users/register", async (req, res) => {
+  try {
+    let { username, email, password, password2 } = req.body;
+    let errors = [];
+  
+    console.log({
+      username,
+      email,
+      password,
+      password2,
+    });
+  
+    if (!username || !email || !password || !password2) {
+      errors.push({ message: "Please enter all fields" });
+    }
+  
+    if (password.length < 6) {
+      errors.push({ message: "Password must be a least 6 characters long" });
+    }
+  
+    if (password !== password2) {
+      errors.push({ message: "Passwords do not match" });
+    }
+  
+    if (errors.length > 0) {
+      res.render("register", { errors, username, email, password, password2 });
+    } else {
+      let hashedPassword = await bcrypt.hash(password, 10);
+      console.log(hashedPassword);
+    }
+  } catch (error) {
+      console.log(error);
+      res.status(500).json({
+          message: "Revisa bien.. algo salió mal."
+      })
+  }
+});
+
+app.use("/api/sucursales", sucursalRoutes);
+app.use("/api/relacionados", relacionadoRoutes);
+app.use("/api/solicitudes", solicitudRoutes);
 
 export default app;
--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,24 @@
 import Sequelize from 'sequelize';
+// const { Pool } = require("pg");
+
+require('dotenv').config();
+
+const isProduction = process.env.NODE_ENV === "production";
+
+// const connString = `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
+// const pool = new Pool({
+//   connString: isProduction? process.env.DATABASE_URL : connString
+// });
+
+// module.exports = { pool };
 
 export const sequelize = new Sequelize(
-    'banke',
-    'postgres',
-    'Maxsteel2!',
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASS,
     {
-        host: 'localhost',
+        host: process.env.DB_HOST,
         dialect: 'postgres',
         pool: {
             max: 5,

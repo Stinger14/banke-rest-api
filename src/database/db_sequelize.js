ab4643c070debca67af30c94778594fa4e2d5952
Maxly Garcia <maxgarcia6@gmail.com>
2020-11-10 23:59:38 -0400

    Change sequilize string to config.

--- src/database/db_sequelize.js
+++ src/database/db_sequelize.js
@@ -1,25 +1,25 @@
 import Sequelize from 'sequelize';
 require('dotenv').config();
 
-export const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`);
+// export const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`);
 
 // Sequelize orm init. (Default connection)
-// export const sequelize = new Sequelize(
-//     process.env.DB_DATABASE,
-//     process.env.DB_USER,
-//     process.env.DB_PASS,
-//     {
-//         host: process.env.DB_HOST,
-//         dialect: 'postgres',
-//         pool: {
-//             max: 5,
-//             min: 0,
-//             require: 30000,
-//             idle: 10000
-//         },
-//         logging: false
-//     }
-// )
+export const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        pool: {
+            max: 5,
+            min: 0,
+            require: 30000,
+            idle: 10000
+        },
+        logging: false
+    }
+)
 
 sequelize
   .authenticate()

ff608c8d9d921883269ef83e74854fdd22de27a1
Maxly Garcia <maxgarcia6@gmail.com>
2020-11-11 11:32:09 -0400

    Actualizar conexión db para Heroku.

--- src/database/db_sequelize.js
+++ src/database/db_sequelize.js
@@ -1,40 +1,19 @@
 import Sequelize from 'sequelize';
 // require('dotenv').config();
 
-export const sequelize = new Sequelize("postgres://qunmnvmiikmgpl:79b5b6db5f7df3339671f655ae376ed1d051f20be449c8d4393e970da40dff45@ec2-54-161-150-170.compute-1.amazonaws.com:5432/dbuqa2i2eldd6j");
+export const sequelize = new Sequelize(`postgres://process.env.DB_USER:process.env.DB_PASS@process.env.DB_HOST:process.env.DB_PORT/process.env.DB_DATABASE`);
 
-<<<<<<< HEAD
-
-// Sequelize orm init. (Default connection)
 // export const sequelize = new Sequelize(
-//     process.env.DB_DATABASE,
-//     process.env.DB_USER,
-//     process.env.DB_PASS,
-//     {
-//         host: process.env.DB_HOST,
-//         dialect: 'postgres',
-//         pool: {
-//             max: 5,
-//             min: 0,
-//             require: 30000,
-//             idle: 10000
-//         },
-//         logging: false
-//     }
-// )
-=======
-export const sequelize = new Sequelize(
-  "postgres://postgres:Maxsteel2!@localhost:5432/banke", {
-     host:"localhost",
-     dialect:'postgres',
-     pool:{
-           max:5,
-           min:0,
-           idle:10000
-     },
-     logging: false
-   });
->>>>>>> dev
+//   "postgres://postgres:Maxsteel2!@localhost:5432/banke", {
+//      host:"localhost",
+//      dialect:'postgres',
+//      pool:{
+//            max:5,
+//            min:0,
+//            idle:10000
+//      },
+//      logging: false
+//    });
 
 // sequelize
 //   .authenticate()
--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import app from './app';
 import "@babel/polyfill"
 
 async function main() {
-<<<<<<< HEAD
-    await app.listen(4000);
-=======
     await app.listen(process.env.PORT || 4000);
     // await app.listen(3000);
->>>>>>> dev
     console.log('Server on port 4000');
 };
 

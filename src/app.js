8230b6a4d922c2bc339d5676d1555e38e886b118
Maxly Garcia <maxgarcia6@gmail.com>
2020-11-18 22:46:18 -0400

    Add middleware for CORS browser errors.

--- src/app.js
+++ src/app.js
@@ -31,6 +31,17 @@ initializePassport(passport);
 
 // Middlewares
 
+
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
+
 // set appropriate HTTP headers 
 //that help protect your app 
 //from well-known web vulnerabilities.
